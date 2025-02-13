38bad5c2393cd9607a2b11cb38f9ce47a22197df
Sandesh Deshmukh <sandesh.deshmukh@jithvar.com>
2025-02-13 15:22:09 +0530

    again fix

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,15 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import HeroSection from "@/section/HeroSection";
-import HeroSection2 from "@/section/HeroSection2";
-import Course from "@/section/Course";
-import Testimonial from "@/section/Testimonial";
-import Help from "@/section/Help";
-import Teachers from "@/section/Teachers";
-import Pricing from "@/section/Pricing";
-import Updates from "@/section/Updates";
-import FAQ from "@/section/FAQ";
+import HeroSection from "../section/HeroSection";
+import HeroSection2 from "../section/HeroSection2";
+import Course from "../section/Course";
+import Testimonial from "../section/Testimonial";
+import Help from "../section/Help";
+import Teachers from "../section/Teachers";
+import Pricing from "../section/Pricing";
+import Updates from "../section/Updates";
+import FAQ from "../section/FAQ";
 
 
 export default function Home() {
