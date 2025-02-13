2338b3a742a41d8895f7c5894fb1a8021da8c9d2
Sandesh Deshmukh <sandesh.deshmukh@jithvar.com>
2025-02-13 15:27:09 +0530

    fix again

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import NavBar from '@/components/NavBar';  // If using alias
-import Footer from '@/components/Footer';
+import NavBar from '../components/navbar';  // If using alias
+import Footer from '../components/footer';
 
 
 
