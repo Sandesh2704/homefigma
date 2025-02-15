1813d837380fc6c53d6d8b78afdba9dfdd94b042
Sandesh Deshmukh <sandesh.deshmukh@jithvar.com>
2025-02-15 11:42:03 +0530

    joins us  add in tutors pages

--- src/app/tutorslisting/page.jsx
+++ src/app/tutorslisting/page.jsx
@@ -4,6 +4,7 @@ import CheckboxList from '@/components/checkBoxlist';
 import PageHeader from '@/components/pageheader'
 import Paragraph from '@/components/paragraph';
 import Section from '@/components/section'
+import SectionTitle from '@/components/sectiontitle';
 import classcourses from '@/deta/class';
 import team from '@/deta/team';
 import Image from 'next/image';
@@ -12,7 +13,6 @@ import { BsSearch } from 'react-icons/bs';
 import { FaRegFolder, FaRegHeart, FaStar } from 'react-icons/fa';
 
 export default function page() {
-
 
     const locations = ["All", "Default", "Onsite", "Online"];
     const courses = ["Joshua Brown", "John Smith", "Eliza Mic", "Holly Hunter", "Sammy King"];
@@ -31,8 +31,9 @@ export default function page() {
 
     return (
         <>
-            <PageHeader title="Our Tutors" />
-            <div>
+            <main>
+                <PageHeader title="Our Tutors" />
+
                 <Section>
                     <div className='grid grid-cols-12 gap-6 xl:gap-8'>
 
@@ -75,8 +76,8 @@ export default function page() {
                             <div className='grid md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-5'>
                                 {team.map((items, index) => (
                                     <div key={index} className="rounded-3xl px-3 lg:px-[18px] py-3  lg:py-[22px] border  border-[#01162773]/45 hover:border-primary hover:border-b-8 hover:border-2 duration-150 ">
-                                       <div className='relative'>
-                                       <Image
+                                        <div className='relative'>
+                                            <Image
                                                 src={items.img}
                                                 alt={items.name}
                                                 className="w-full h-[302px] object-cover rounded-2xl"
@@ -86,9 +87,9 @@ export default function page() {
                                             <div className="absolute ">
 
                                             </div>
-                                       </div>
-                                        
-                                      
+                                        </div>
+
+
                                         <div className='text-center flex flex-col gap-y-2 mt-4'>
                                             <h2 className="text-xl lg:text-2xl font-semibold">{items.name}</h2>
                                             <h3 className='text-base md:text-lg font-normal'>{items.role}</h3>
@@ -99,7 +100,37 @@ export default function page() {
                         </div>
                     </div>
                 </Section>
-            </div>
+
+
+                <Section color="#011627">
+                    <div className="grid lg:grid-cols-2 h-fit gap-8 lg:gap-12">
+                        <Image
+                            src="/assests/heroSection/00bec2872c1d0f42d1c3565a5995a521.png"
+                            alt="join us"
+                            className="w-full h-[300px] lg:h-[352px] object-cover rounded-2xl order-last md:order-first"
+                            width={200}
+                            height={100}
+                            priority />
+                        <div className='order-first md:order-last h-full flex items-center'>
+                            <div>
+                            <SectionTitle title="Join Us " />
+
+
+                                <p className=' text-white  text-base md:text-[18px] leading-[1.8rem] mt-5 mb-10'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+    </p>
+           
+                            <button>
+                                <Button title="Appy Within" />
+                            </button>
+                            </div>
+                         
+                        </div>
+                    </div>
+                </Section>
+            </main>
+
+
         </>
     )
 }
